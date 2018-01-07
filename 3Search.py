e7cdc2ed00a8988f1e8cb50b758cc1aa713f76a7
jordanaron22 <jordanaron22@gmail.com>
2018-01-07 13:45:12 -0600

    Calculates all combinations and power distributions

--- 3Search.py
+++ 3Search.py
@@ -1,5 +1,31 @@
 def main():
+
+    def power_calc(local_winning_combo):
+        list_a = []
+        list_b = []
+        list_c = []
+
+        for combo in local_winning_combo:
+            for i in range(0,len(combo)):
+                check = combo.replace(combo[i], "")
+                if check not in local_winning_combo:
+                    if combo[i] == "A":
+                        list_a.append(1)
+                    elif combo[i] == "B":
+                        list_b.append(1)
+                    else:
+                        list_c.append(1)
+
+        denom = len(list_a)+len(list_b)+len(list_c)
+        power_a = len(list_a)/denom
+        power_b = len(list_b)/denom
+        power_c = len(list_c)/denom
+
+        power_list = [power_a, power_b, power_c]
+        return power_list
+
     winning_combos = []
+    winning_combos_power =[]
     for a in range(1,101):
         for b in range(0,100-a+1):
             for c in range(0,100-a-b+1):
@@ -9,7 +35,7 @@ def main():
                     #print("C is ", c)
                     #print("")
 
-                    for i in range(1,101):
+                    for i in range(1,100):
                         local_winning_combo = []
                         if a+b+c > i:
                             #print("Winning combination: ABC")
@@ -38,6 +64,16 @@ def main():
                         if not local_winning_combo in winning_combos:
                             winning_combos.append(local_winning_combo)
 
+                            power_dist = power_calc(local_winning_combo)
+                            if power_dist not in winning_combos_power:
+                                winning_combos_power.append(power_dist)
+
+    print("The possible winning combinations are:")
     print(winning_combos)
+    print("")
+    print("There are ",len(winning_combos_power), " different power distributions")
+    print("")
+    print("The possible power distributions are: ")
+    print(winning_combos_power)
 
 main()
--- 4Search.py
+++ 4Search.py
@@ -1,17 +1,63 @@
 def main():
+
+    #These lists will have the final answers in them
     winning_combos = []
+    winning_combos_power =[]
+
+    def summary():
+        print("The possible winning combinations are:")
+        print(winning_combos)
+        print("")
+        print("There are ", len(winning_combos_power), " different power distributions")
+        print("")
+        print("The possible power distributions are: ")
+        print(winning_combos_power)
+
+    #Calculates power from list of winning combinations
+    def power_calc(local_winning_combo):
+        #Used later to tally
+        list_a = []
+        list_b = []
+        list_c = []
+        list_d = []
+
+        #iterates through each winning coalition
+        for combo in local_winning_combo:
+            #iterates through each member of a winning coalition
+            for i in range(0, len(combo)):
+                #iteratively removes each member and checks if winning coalition goes to losing
+                check = combo.replace(combo[i], "")
+                if check not in local_winning_combo:
+                    if combo[i] == "A":
+                        list_a.append(1)
+                    elif combo[i] == "B":
+                        list_b.append(1)
+                    elif combo[i] == "C":
+                        list_c.append(1)
+                    else:
+                        list_d.append(1)
+
+        denom = len(list_a) + len(list_b) + len(list_c) + len(list_d)
+        power_a = len(list_a)/denom
+        power_b = len(list_b)/denom
+        power_c = len(list_c)/denom
+        power_d = len(list_d)/denom
+
+        power_list = [power_a, power_b, power_c, power_d]
+        return power_list
+
+    #Calculates every possible combination of values for ABCD
     for a in range(1, 101):
         for b in range(0, 100 - a + 1):
             for c in range(0, 100 - a - b + 1):
                 for d in range(0,100 - a - b - c +1):
-                    if a + b + c + d == 100 and a >= b >= c >= d:
-                        # print("A is ", a)
-                        # print("B is ", b)
-                        # print("C is ", c)
-                        # print("")
 
+                    #Only look at valid combinations
+                    if a + b + c + d == 100 and a >= b >= c >= d:
+                        #Every possible threshold value
                         for i in range(1, 100):
                             local_winning_combo = []
+                            #Checks if coalition is a winning one
                             if a + b + c + d > i:
                                 local_winning_combo.append("ABCD")
                             if a + b + c > i:
@@ -45,12 +91,16 @@ def main():
 
 
 
-                            # print(local_winning_combo)
-
+                            #adds to list of winning coalitions
                             if not local_winning_combo in winning_combos:
                                 winning_combos.append(local_winning_combo)
 
-    print(winning_combos)
+                                #adds to list of power distributions
+                                power_dist = power_calc(local_winning_combo)
+                                if power_dist not in winning_combos_power:
+                                    winning_combos_power.append(power_dist)
 
+
+    summary()
 
 main()
